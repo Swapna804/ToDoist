fb72dc7b9493f926bb024e760d09805c7c0499ae
Pranav <pranavmendi@gmail.com>
2020-12-24 21:35:46 +0530

    Added a POST request for the input form

--- index.js
+++ index.js
@@ -3,38 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
     var today= new Date();
-    var currentDay=today.getDay();
-    var day="";
-    switch (currentDay) {
-        case 0:
-            day="Sunday"
-            break;
-        case 1:
-            day="Monday"
-            break;
-        case 2:
-            day="Tuesday"
-            break;
-        case 3:
-            day="Wednesday"
-            break;
-        case 4:
-            day="Thusday"
-            break;
-        case 5:
-            day="Friday"
-            break;
-        case 6:
-            day="Saturday"
-            break;
-        default:
-            console.log("Valid day was not added.");
+    var options={
+        day: "numeric",
+        weekday: "long",
+        month: "long",
     }
-    res.render("list",{day:day});
+    var date= today.toLocaleDateString("en-US",options);
+    res.render("list",{date:date});
+});
+
+app.post("/", function(req, res){
+    console.log(req.body.newItem);
 });
 
 app.listen(3000, function(){
