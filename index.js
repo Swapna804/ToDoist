feb1b4a6a5f2cca7e10b7fc4bd99b42ff8fd4d13
Pranav <pranavmendi@gmail.com>
2020-12-25 23:44:12 +0530

    Added css style to the list and updated get request for work list

--- index.js
+++ index.js
@@ -3,29 +3,48 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.set("view engine","ejs");
-var items=[];
+app.set("view engine", "ejs");
+let items = [];
+let workItems = [];
 
-app.get("/", function(req, res){
-    var today= new Date();
-    var options={
+app.get("/", function(req, res) {
+    let today = new Date();
+    let options = {
         day: "numeric",
         weekday: "long",
         month: "long",
     }
-    var date= today.toLocaleDateString("en-US",options);
-    res.render("list",{date:date, items:items});
+    let date = today.toLocaleDateString("en-US", options);
+    res.render("list", { listTitle: date, items: items });
 });
 
-app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
-
-    res.redirect("/")
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    console.log(item);
+    if(req.body.list === 'Work'){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, function(){
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", items: workItems });
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+});
+
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
